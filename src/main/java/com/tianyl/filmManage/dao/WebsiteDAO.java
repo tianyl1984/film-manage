8d8b7aee4471d655c2f04d045333a9634ab14bb0
tianyl1984 <tianyl1984@qq.com>
2016-08-18 22:56:54 +0800

    add filmsite order

--- src/main/java/com/tianyl/filmManage/dao/WebsiteDAO.java
+++ src/main/java/com/tianyl/filmManage/dao/WebsiteDAO.java
@@ -8,7 +8,7 @@ import com.tianyl.filmManage.util.sql.JdbcUtil;
 public class WebsiteDAO {
 
 	public static List<Website> findAll() {
-		String sql = "select * from website";
+		String sql = "select * from website order by orderNum";
 		return JdbcUtil.query(sql, WebsiteRowMapper.getInstance());
 	}
 
